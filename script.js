db58abf91ee95c962b250939fe684d3e75deea30
rsridoy-000 <rsridoykhan11111@gmail.com>
2025-02-17 20:29:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "GAMER", "PHOTOGRAPHER "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".multiple-text2", {
+    strings: ["PORTFOLIO...", "R S R I D O Y..."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+})
